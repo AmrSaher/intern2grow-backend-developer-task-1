a67212f77284c2df705d7eb0899a92b82908af7c
Amr Saher <amrosaher1234@gmail.com>
2023-10-27 22:43:00 +0200

    create articles controller and endpoints

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ArticleRequest;
+use App\Models\Article;
 use Illuminate\Http\Request;
 
 class ArticlesController extends Controller
@@ -11,54 +13,64 @@ class ArticlesController extends Controller
      */
     public function index()
     {
-        //
-    }
+        $articles = Article::latest()->get();
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
+        return response()->json($articles);
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(ArticleRequest $request)
     {
-        //
+        Article::create($request->all());
+
+        return response()->json([
+            'message' => 'Article created successfully!',
+        ]);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Article $article)
     {
-        //
-    }
+        if ($article) return response()->json($article);
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
+        return response()->json([
+            'message' => 'Article not found.',
+        ], 404);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(ArticleRequest $request, Article $article)
     {
-        //
+        if (!$article) return response()->json([
+            'message' => 'Article not found.'
+        ], 404);
+
+        $article->update($request->all());
+
+        return response()->json([
+            'message' => 'Article created successfully!',
+        ]);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Article $article)
     {
-        //
+        if (!$article) return response()->json([
+            'message' => 'Article not found.',
+        ], 404);
+
+        $article->delete();
+
+        return response()->json([
+            'message' => 'Article deleted successfully!',
+        ]);
     }
 }
